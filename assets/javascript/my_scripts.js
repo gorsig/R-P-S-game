ce03e1b064f4bc2c5bc9833ea841856ed4cd9a54
gorsig <sigeskog@hotmail.se>
2021-07-26 14:58:04 +0000

    Add modal box to highscores button and content and style for it

--- assets/javascript/my_scripts.js
+++ assets/javascript/my_scripts.js
@@ -1,25 +1,33 @@
-// Get the modal, code from w3school.com
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("rules");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// Get the modal, most of the code from w3school.com
+let modal = document.getElementById("myModal");
+let modalTwo = document.getElementById("myModalTwo")
+let btnRules = document.getElementById("rules"); // Get the button that opens the modal
+let btnHighscores = document.getElementById("highscores"); // Get the button that opens the modal
+let span = document.getElementsByClassName("close")[0]; // Get the <span> element that closes the modal
+let span2 = document.getElementsByClassName("close")[1];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+btnRules.onclick = function () {
+    modal.style.display = "block";
+}
+btnHighscores.onclick = function () {
+    modalTwo.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+span.onclick = function () {
+    modal.style.display = "none";
+}
+span2.onclick = function() {
+    modalTwo.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+    if (event.target == modalTwo) {
+        modalTwo.style.display = "none";
+    }
 }
