9adbf6186feda221462b15c99c6b696ea993cad6
gorsig <sigeskog@hotmail.se>
2021-08-04 18:43:02 +0000

    removed non functional buttons

--- assets/javascript/my_scripts.js
+++ assets/javascript/my_scripts.js
@@ -1,31 +1,19 @@
 // some of the code for modals from w3school.com
 let modal = document.getElementById("myModal");
-let modalTwo = document.getElementById("myModalTwo")
 let btnRules = document.getElementById("rules");
-let btnHighscores = document.getElementById("highscores");
 let span = document.getElementsByClassName("close")[0];
-let span2 = document.getElementsByClassName("close")[1];
 // When the user clicks on the button, open the modal
 btnRules.onclick = function () {
     modal.style.display = "block";
-}
-btnHighscores.onclick = function () {
-    modalTwo.style.display = "block";
 }
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
     modal.style.display = "none";
 }
-span2.onclick = function () {
-    modalTwo.style.display = "none";
-}
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
-    }
-    if (event.target == modalTwo) {
-        modalTwo.style.display = "none";
     }
 }
 // getting the elements
@@ -65,9 +53,7 @@ let game = () => {
             showCompLastchoice.className = randomClasses[randomNum]
             resetBtn.onclick = () => {
                 location.reload()
-            }
-            // Show the last 3 rounds
-            
+            }       
             // Game score
             // if it is a tie
             if(showUserChoice.className === showComputerChoice.className){
